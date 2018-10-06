c43fae7a5118c981cc619d5370b60ecf9f8c3c9a
Unknown <maryshipkova@gmail.com>
2018-10-06 21:22:35 +0300

    part 2 refactorin'

--- 1.responsive-design/sensor-input.js
+++ 1.responsive-design/sensor-input.js
@@ -1,6 +1,7 @@
 const element = document.querySelectorAll('.card__data__image--img')[1];
 const zoomField = document.querySelectorAll('.settings--zoom')[1];
 const brightnessField = document.querySelectorAll('.settings--brightness')[1];
+const translateTrack = document.querySelector('.settings--range');
 
 const TRANSFORM_PROPERTIES = {
     'translateX': 0,
@@ -11,19 +12,23 @@ const TRANSFORM_PROPERTIES = {
 const INITIAL_CONDITIONS = {
     'translateX': "",
     'translateY': "",
-    'rotation': "",
+    'brightness': "",
 }
 class sensorInputHandler {
-    constructor(element, INITIAL_CONDITIONS, transformProperties, zoomField, brightnessField) {
+    constructor(element, INITIAL_CONDITIONS, transformProperties, zoomField, brightnessField, translateTrack) {
         this._element = element;
         this._transformProperties = transformProperties;
         this._prevConditions = INITIAL_CONDITIONS;
         this._startProperties = JSON.parse(JSON.stringify(transformProperties));
         this._zoomField = zoomField;
         this._brightnessField = brightnessField;
+        this._translateTrack = translateTrack;
 
     }
 
+    _updateTrack(newCondition){
+        this._translateTrack.value = parseInt(newCondition);
+    }
     _updateField(field, newCondition) {
         field.innerHTML = newCondition;
     }
@@ -42,53 +47,38 @@ class sensorInputHandler {
         this._element.addEventListener('touchstart', this._resetConditions);
         this._element.addEventListener('gesturechange', (event) => {
             this._handleRotation(event.rotation);
-            if(event.scale > 1.0){
+            if (event.scale > 1.0) {
                 this._updateProperty('scale', event.scale);
-                this._updateField(this._zoomField,(event.scale*100).toPrecision(3));
+                this._updateField(this._zoomField, (event.scale * 100).toPrecision(3));
             }
         });
-
 
         element.addEventListener('touchmove', (event) => {
             event.preventDefault();
-            if (event.touches.length === 1) { // && transformProperties.scale > 1.0
-                if (!this._prevConditions.translateX || !this._prevConditions.translateY) {
-                    this._prevConditions.translateX =  event.touches[0].clientX;
-                    this._prevConditions.translateY =  event.touches[0].clientY;
-                    return;
-                }
-                // let newXValue = this._calcChanges('translateX', event.touches[0].clientX);
-                // console.log('X', newXValue, event.touches[0].clientX, this._prevConditions.translateX);
-                // if (newXValue) {
-                //     this._updateProperty('translateX', this._prevConditions.translateX);
-                // }
-                // let newYValue = this._calcChanges('translateY', event.touches[0].clientY);
-                // console.log('X', newXValue);
-                // if (newYValue) {
-                //     this._updateProperty('translateY', newYValue);
-                // }
+            if (event.touches.length === 1 && this._transformProperties.scale > 1.0) {
 
-                let clientX = event.touches[0].clientX;
-                let clientY = event.touches[0].clientY;
-                if (! this._prevConditions.translateX  || !this._prevConditions.translateY) {
-                     this._prevConditions.translateX  = clientX;
-                    this._prevConditions.translateY = clientY;
-                    return;
-                }
-                // console.log(this._transformProperties);
-                if (Math.abs(clientX -  this._prevConditions.translateX ) > 3) {
-                    this._updateProperty('translateX', ( this._prevConditions.translateX  < clientX) ? this._transformProperties.translateX + 2 : this._transformProperties.translateX - 2);
-                    this._prevConditions.translateX  = clientX;
-                }
-                if (Math.abs(clientY - this._prevConditions.translateY) > 3) {
-                    this._updateProperty('translateY', (this._prevConditions.translateY < clientY) ? this._transformProperties.translateY + 2 : this._transformProperties.translateY - 2);
-                    this._prevConditions.translateY  = clientY;
-                }
-
+                this._calcChanges('translateX', event.touches[0].clientX);
+                this._updateTrack(this._transformProperties.translateX.toPrecision(3));
+                this._calcChanges('translateY', event.touches[0].clientY);
             }
 
         });
+
     }
+    _calcChanges(conditionName, eventCondition) {
+        if (!this._prevConditions[conditionName]) {
+            this._prevConditions[conditionName] = eventCondition;
+            return;
+        }
+        if (Math.abs(eventCondition - this._prevConditions[conditionName]) > 3) {
+            let newConditionValue = (this._prevConditions[conditionName] < eventCondition) ? this._transformProperties[conditionName] + 2 : this._transformProperties[conditionName] - 2;
+
+            this._updateProperty(conditionName, newConditionValue);
+            this._prevConditions[conditionName] = eventCondition;
+
+        }
+    }
+
     _resetProperties() {
         _updateField(this._zoomField, 100);
         _updateField(this._brightnessField, 100);
@@ -103,30 +93,13 @@ class sensorInputHandler {
     }
 
     _handleRotation(eventRotation) {
-        if (!this._prevConditions.rotation) {
-            this._prevConditions.rotation = eventRotation;
-            return;
-        }
-        if (Math.abs(eventRotation - this._prevConditions.rotation) > 3) {
-            this._updateProperty('brightness', (this._prevConditions.rotation < eventRotation) ? this._transformProperties.brightness + 2 : this._transformProperties.brightness - 2);
-            this._updateField(this._brightnessField, this._transformProperties.brightness);
-            this._prevConditions.rotation  = eventRotation;
-        }
-        // let newBrightnessValue = this._calcChanges('rotation', eventRotation);
-        // if (newBrightnessValue) {
-        //     this._updateProterty('brightness', newBrightnessValue)
-        //     this._updateField(this._brightnessField, newBrightnessValue);
-        // }
+        this._calcChanges('brightness', eventRotation);
+        this._updateField(this._brightnessField, this._transformProperties.brightness);
+
     }
 
-    _calcChanges(conditionName, eventCondition) {
-        if (Math.abs(eventCondition - this._prevConditions[conditionName]) > 3) {
-            let newConditionValue = (this._prevConditions[conditionName] < eventCondition) ? this._prevConditions[conditionName] + 2 : this._prevConditions[conditionName] - 2;
-            this._prevConditions[conditionName] = eventCondition;
-            return newConditionValue;
-        }
-    }
+
 }
 
-let ImageSensorInputHandler = new sensorInputHandler(element, INITIAL_CONDITIONS, TRANSFORM_PROPERTIES, zoomField, brightnessField);
+let ImageSensorInputHandler = new sensorInputHandler(element, INITIAL_CONDITIONS, TRANSFORM_PROPERTIES, zoomField, brightnessField, translateTrack);
 ImageSensorInputHandler.setEventHandlers();
