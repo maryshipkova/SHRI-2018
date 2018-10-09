9f82bdff07d6ebaecdda23c527b586356e465841
Unknown <maryshipkova@gmail.com>
2018-10-10 00:20:34 +0300

    pointer events

---
+++ multimedia/sstv.js
@@ -0,0 +1,13 @@
+// let video1 = document.querySelector('#video-1');
+
+// let source = fetch('http://localhost:3102').then((response)=>{
+
+
+//     return response.text();
+// }).then((resp)=>{
+//     let urls = resp.match(/http.*/g).slice(2);
+//     video1.src = urls[0];
+
+
+// console.log(video1);
+// })
