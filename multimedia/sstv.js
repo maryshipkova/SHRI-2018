331431cc973bac050d67c309fbb00d52b89e2778
Maria Shipkova <maryshipkova@gmail.com>
2018-10-11 16:18:40 +0300

    added inputs

--- multimedia/sstv.js
+++ multimedia/sstv.js
@@ -1,13 +1,35 @@
 // let video1 = document.querySelector('#video-1');
+const CONTROLS = {
+    'brightness':{
+        view: document.querySelector('#input-brightness'),
+        value:100
+    },
+    'contrast':{
+        view: document.querySelector('#input-contrast'),
+        value:100
+    }
+}
 
-// let source = fetch('http://localhost:3102').then((response)=>{
+let videoList = [];
+document.querySelectorAll('.sstv__video').forEach((video,num) =>{
+    let expandBtn = document.querySelector(``);
+    videoList.push({
+        element:document.querySelector(`#video-${item}`),
+        expandBtn:expandBtn
+    });
+    
+    expandBtn.addEventListener('click', expandVideo);
+});
 
+function expandVideo(){
+    let SSTVControlSystem = new SSTVControl(CONTROLS,this);
 
-//     return response.text();
-// }).then((resp)=>{
-//     let urls = resp.match(/http.*/g).slice(2);
-//     video1.src = urls[0];
+}
+class SSTVControl{
+    constructor(properties,video){
+        this._properties = properties;
+        this._video = video;
+        this._closeBtn =  document.querySelector(`#close-btn`);
+    }
+}
 
-
-// console.log(video1);
-// })
