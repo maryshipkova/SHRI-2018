1f81892a59ae63ea014f5f7b096d147119594fd2
Unknown <maryshipkova@gmail.com>
2018-10-11 22:41:58 +0300

    1(no animation) and 2 tasks

--- multimedia/sstv.js
+++ multimedia/sstv.js
@@ -1,35 +1,73 @@
-// let video1 = document.querySelector('#video-1');
 const CONTROLS = {
-    'brightness':{
-        view: document.querySelector('#input-brightness'),
-        value:100
-    },
-    'contrast':{
-        view: document.querySelector('#input-contrast'),
-        value:100
+    'brightness': document.querySelector('#input-brightness'),
+    'contrast' :document.querySelector('#input-contrast')
+    
+}
+
+
+class SSTVControl {
+    constructor(videoSelector) {
+
+        this._videoList = [];
+        document.querySelectorAll(videoSelector).forEach((video, num) => {
+            let expandBtn = document.querySelectorAll(`.sstv__btn-expand`)[num];
+            this._videoList.push({
+                element: document.querySelector(`#video-${num+1}`),
+                expandBtn: expandBtn,
+            });
+        });
+
+        this._videoList.forEach(videoItem=>{
+            videoItem.expandBtn.addEventListener('click', this._expandVideo.bind(videoItem));
+        });
+      
+    }
+
+    _expandVideo() {
+        this._expandedCamera = new ExpandedCamera(CONTROLS, this);
+    }
+    closeVideo(){
+        delete this._expandedCamera;
     }
 }
-
-let videoList = [];
-document.querySelectorAll('.sstv__video').forEach((video,num) =>{
-    let expandBtn = document.querySelector(``);
-    videoList.push({
-        element:document.querySelector(`#video-${item}`),
-        expandBtn:expandBtn
-    });
-    
-    expandBtn.addEventListener('click', expandVideo);
-});
-
-function expandVideo(){
-    let SSTVControlSystem = new SSTVControl(CONTROLS,this);
-
-}
-class SSTVControl{
-    constructor(properties,video){
+class ExpandedCamera {
+    constructor(properties, video) {
         this._properties = properties;
         this._video = video;
-        this._closeBtn =  document.querySelector(`#close-btn`);
+        this._closeBtn = document.querySelector(`#close-btn`);
+        this._container = document.querySelector('.sstv__expanded');
+        this._videoContainer = this._video.element.parentElement;
+        this._eventsHandling();
+        this._expand();
+    }
+    _eventsHandling(){
+        this._closeBtn.addEventListener('click', this._close.bind(this));
+        this._properties.brightness.addEventListener('change',()=>this._updateView());
+        this._properties.contrast.addEventListener('change',()=>this._updateView());
+    }
+
+    _updateView(){
+        this._video.element.style.filter = `brightness(${this._properties.brightness.value}%) contrast(${this._properties.contrast.value}%)`;
+        console.log ( this._properties.contrast.value, this._video.element);
+    }
+    _resetView(){
+        this._video.element.style.filter='';
+        this._properties.brightness.value = 100;
+        this._properties.contrast.value = 100;
+    }
+
+    _expand(){
+        this._videoContainer.removeChild(this._video.element);
+        this._container.appendChild(this._video.element);
+        this._video.element.play();
+    }
+    _close(){
+        this._resetView();
+        this._container.removeChild(this._video.element);
+        this._videoContainer.appendChild(this._video.element);
+        this._video.element.play();
+        SSTVControlSystem.closeVideo();
     }
 }
 
+let SSTVControlSystem = new SSTVControl('.sstv__video');
