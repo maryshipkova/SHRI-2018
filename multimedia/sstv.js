11c289f4145aa4d33d0b3a5a415b226058f67542
Maria Shipkova <maryshipkova@gmail.com>
2018-10-14 10:02:50 +0300

    3 task complete

--- multimedia/sstv.js
+++ multimedia/sstv.js
@@ -1,161 +1,173 @@
 class SSTVControl {
-    constructor(videoSelector) {
+	constructor(videoSelector) {
+		this._videoList = [];
+		document.querySelectorAll(videoSelector).forEach((video, num) => {
+			const expandBtn = document.querySelectorAll(".sstv__btn-expand")[num];
+			this._videoList.push({
+				id: num,
+				element: document.querySelector(`#video-${num + 1}`),
+				expandBtn,
+			});
+		});
 
-        this._videoList = [];
-        document.querySelectorAll(videoSelector).forEach((video, num) => {
-            let expandBtn = document.querySelectorAll(`.sstv__btn-expand`)[num];
-            this._videoList.push({
-                element: document.querySelector(`#video-${num+1}`),
-                expandBtn: expandBtn,
-            });
-        });
+		this._videoList.forEach((videoItem) => {
+			videoItem.boundingClientRect = videoItem.element.getBoundingClientRect();
+			videoItem.expandBtn.addEventListener("click", this._expandVideo.bind(videoItem));
+		});
+	}
 
-        this._videoList.forEach(videoItem => {
-            videoItem.expandBtn.addEventListener('click', this._expandVideo.bind(videoItem));
-        });
+	_expandVideo() {
+		this._expandedCamera = new ExpandedCamera(this, CONTROLS, canvas);
+	}
 
-    }
 
-    _expandVideo() {
-        this._expandedCamera = new ExpandedCamera(this, CONTROLS, canvas);
-    }
-
-    closeVideo() {
-
-        delete this._expandedCamera;
-    }
+	closeVideo() {
+		delete this._expandedCamera;
+	}
 }
 
 class ExpandedCamera {
-    constructor(video, properties, canvas) {
-        this._video = video;
-        this._properties = properties;
-        this._canvas = canvas;
-        this._closeBtn = document.querySelector(`#close-btn`);
-        this._container = document.querySelector('.sstv__expanded');
-        this._videoContainer = this._video.element.parentElement;
-        this._analyser = new Analyse(this._video.element);
-        this._eventsHandling();
-        this._expand();
-        this._analyser.draw();
+	constructor(video, properties, canvas) {
+		this._video = video;
+		this._properties = properties;
+		this._canvas = canvas;
+		this._analyser = new Analyse(this._video.element);
+		this._eventsHandling();
+		this._expand();
+		this._analyser.draw();
+	}
 
+	_eventsHandling() {
+		SSTVSELECTORS.closeBtn.addEventListener("click", this._close.bind(this));
+		this._properties.brightness.addEventListener("change", () => this._updateView());
+		this._properties.contrast.addEventListener("change", () => this._updateView());
+	}
 
+	_updateView() {
+		this._video.element.style.filter = `brightness(${this._properties.brightness.value}%) contrast(${this._properties.contrast.value}%)`;
+	}
 
-    }
-    _eventsHandling() {
-        this._closeBtn.addEventListener('click', this._close.bind(this));
-        this._properties.brightness.addEventListener('change', () => this._updateView());
-        this._properties.contrast.addEventListener('change', () => this._updateView());
-    }
+	_resetView() {
+		this._video.element.style.filter = "";
+		this._properties.brightness.value = 100;
+		this._properties.contrast.value = 100;
+		resetCanvas();
+	}
 
-    _updateView() {
-        this._video.element.style.filter = `brightness(${this._properties.brightness.value}%) contrast(${this._properties.contrast.value}%)`;
-    }
+	_expand() {
+		SSTVSELECTORS.videoContainers[this._video.id].removeChild(this._video.element);
+		SSTVSELECTORS.sstvContainer.appendChild(this._video.element);
+		this._animateVideo();
+		this._video.element.muted = false;
+		this._video.element.controls = true;
+		this._video.element.play();
+	}
 
-    _resetView() {
-        this._video.element.style.filter = '';
-        this._properties.brightness.value = 100;
-        this._properties.contrast.value = 100;
-        this._canvas.context.fillStyle = '#000000';
-        this._canvas.context.fillRect(0, 0, this._canvas.element.width, this._canvas.element.height);
-    }
+	_animateVideo() {
+		const sstvBoundingRect = SSTVSELECTORS.sstvContainer.getBoundingClientRect();
 
-    _expand() {
-        this._videoContainer.removeChild(this._video.element);
-        this._container.appendChild(this._video.element);
-        this._video.element.muted = false;
-        this._video.element.controls = true;
-        this._video.element.play();
+		SSTVSELECTORS.sstvContainer.animate([
+			{ // from
+				width: `${this._video.boundingClientRect.width}px`,
+				height: `${this._video.boundingClientRect.height}px`,
+				top: `${this._video.boundingClientRect.top}px`,
+				left: `${this._video.boundingClientRect.left}px`,
+			},
+			{ // to
+				width: `${sstvBoundingRect.width}px`,
+				height: `${sstvBoundingRect.height}px`,
+				top: `${sstvBoundingRect.top}px`,
+				left: `${sstvBoundingRect.left}px`,
+			},
+		], 200);
+	}
 
-
-    }
-    _close() {
-        //TODO:
-        // delete this._analyser; &&cancel animation frame
-        this._resetView();
-        this._container.removeChild(this._video.element);
-        this._videoContainer.appendChild(this._video.element);
-        this._video.element.muted = true;
-        this._video.element.controls = false;
-        this._video.element.play();
-        SSTVControlSystem.closeVideo();
-    }
-
-
-
+	_close() {
+		this._resetView();
+		SSTVSELECTORS.closeBtn.removeEventListener("click", this._close.bind(this));
+		this._analyser.isDrawing = false;
+		SSTVSELECTORS.videoContainers[this._video.id].appendChild(this._video.element);
+		if (SSTVSELECTORS.sstvContainer.children.length) SSTVSELECTORS.sstvContainer.removeChild(SSTVSELECTORS.sstvContainer.children[0]);
+		this._video.element.muted = true;
+		this._video.element.controls = false;
+		this._video.element.play();
+		SSTVControlSystem.closeVideo();
+	}
 }
-
-
+const context = new (window.AudioContext || window.webkitAudioContext)();
+let source = 0;
+let sources = new WeakMap();
 class Analyse {
-    constructor(videoElement) {
+	constructor(videoElement) {
+		this.video = videoElement;
 
-        let AudioContext = window.AudioContext || window.webkitAudioContext;
+		this.node = context.createScriptProcessor(2048, 1, 1);
+		this.analyser = context.createAnalyser();
+		this.analyser.smoothingTimeConstant = 0.3;
+		this.analyser.fftSize = 512;
+		this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
+		this.isDrawing = true;
 
-        //Создание источника
-        this.video = videoElement;
-
-        //Создаем аудио-контекст
-        this.context = new AudioContext();
-        this.node = this.context.createScriptProcessor(2048, 1, 1);
-        //Создаем анализатор
-        this.analyser = this.context.createAnalyser();
-        this.analyser.smoothingTimeConstant = 0.3;
-        this.analyser.fftSize = 512;
-        this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
-
-        //    отправляем на обработку в  AudioContext 
-        this.source = this.context.createMediaElementSource(this.video);
-        //связываем источник и анализатором
-        this.source.connect(this.analyser);
-        //связываем анализатор с интерфейсом, из которого он будет получать данные
-        this.analyser.connect(this.node);
-        //Связываем все с выходом
-        this.node.connect(this.context.destination);
-        this.source.connect(this.context.destination);
-        this.ifFilled = false;
-
-    }
-
-
-    draw() {
-
-        requestAnimationFrame(() => this.draw());
-        this.analyser.getByteFrequencyData(this.dataArray);
-
-        let barHeight = average(this.dataArray);
-
-        if (!this.ifFilled) {
-            canvas.context.fillStyle = '#FF0000';
-            canvas.context.fillRect(0, canvas.element.height - barHeight, canvas.element.width, barHeight);
-        } else {
-            canvas.context.fillStyle = '#000000';
-            canvas.context.fillRect(0, 0, canvas.element.width, canvas.element.height);
-        }
+		if (!source) {
+			source = context.createMediaElementSource(videoElement);
+			sources.set(videoElement,source);
+		} else {
+			if(sources.get(videoElement)){
+				source = sources.get(videoElement);
+			}else{
+				const options = {
+					mediaElement: videoElement,
+				};
+				source = new MediaElementAudioSourceNode(context, options);
+				sources.set(videoElement,source);
+			}
+		}
+		source.connect(this.analyser);
+		this.analyser.connect(this.node);
+		this.node.connect(context.destination);
+		source.connect(context.destination);
+	}
 
 
-        this.ifFilled = !this.ifFilled;
+	draw() {
+		if (!this.isDrawing) {
+			return;
+		}
+		requestAnimationFrame(() => this.draw());
+		this.analyser.getByteFrequencyData(this.dataArray);
 
+		const barHeight = average(this.dataArray);
 
-    }
+		resetCanvas();
+		canvas.context.fillStyle = "#FF0000";
+		canvas.context.fillRect(0, canvas.element.height - barHeight, canvas.element.width, barHeight);
+	}
+}
+function resetCanvas() {
+	canvas.context.fillStyle = "#FFFFFF";
+	canvas.context.fillRect(0, 0, canvas.element.width, canvas.element.height);
 }
 
 const CONTROLS = {
-    'brightness': document.querySelector('#input-brightness'),
-    'contrast': document.querySelector('#input-contrast')
+	brightness: document.querySelector("#input-brightness"),
+	contrast: document.querySelector("#input-contrast"),
 
-}
+};
+const SSTVSELECTORS = {
+	closeBtn: document.querySelector("#close-btn"),
+	sstvContainer: document.querySelector(".sstv__expanded"),
+	videoContainers: document.querySelectorAll(".sstv__video__container"),
+};
 
-let SSTVControlSystem = new SSTVControl('.sstv__video');
+let SSTVControlSystem = new SSTVControl(".sstv__video");
 
-let canvasElement = document.getElementById("analyzer")
+const canvasElement = document.getElementById("analyzer");
 let canvas = {
-    element: canvasElement,
-    context: canvasElement.getContext("2d")
-}
+	element: canvasElement,
+	context: canvasElement.getContext("2d"),
+};
 
-canvas.context.fillStyle = 'rgb(0, 0, 0)';
-canvas.context.fillRect(0, 0, canvas.element.width, canvas.element.height);
-
+resetCanvas();
 
 
 const average = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
